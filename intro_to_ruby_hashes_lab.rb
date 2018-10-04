c4e10bcfa1cd6588ff41a2ac42aae79c22627bdd
Kelly Sheddy <jungaka07@gmail.com>
2018-10-04 00:05:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,13 +8,15 @@ def actor
 end
 
 def monopoly
-	:monopoly = {railroads: {}}
+	monopoly = {railroads: {}}
 
 end
 
 def monopoly_with_second_tier
-  :railroads =
-      {pieces: 4, rent_in_dollars: {}} 
+  monopoly = {railroads: 
+    {pieces: 4, names: {}, 
+    rent_in_dollars: {}} 
+  }
 end
 
 def monopoly_with_third_tier
